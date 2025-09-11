344e46f71ff68094307e11c1057420b656abdd4a
siva <sivabalans.career@gmail.com>
2025-09-11 12:53:56 +0530

    CategoryAdmin added

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import register
+
+from backend.models import Category
+
 
 # Register your models here.
+@register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
